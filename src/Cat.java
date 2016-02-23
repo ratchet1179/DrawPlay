f30fdba4120f1c79c973a44bbe5ae284c9437289
Shawn Andrew <sandrew@mines.edu>
2016-02-23 00:36:00 -0700

    Lowered the cats eyes

--- src/Cat.java
+++ src/Cat.java
@@ -31,8 +31,8 @@ public class Cat {
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
 		// Draw the eyes
 		g2.setColor(Color.orange);
-		x = catX + EYE_X; 
-		y = catY + EYE_Y;
+		x = catX + EYE_X ; 
+		y = catY + EYE_Y + 15;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		x += EYE_SEPARATION;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
